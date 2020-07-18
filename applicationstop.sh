d0b241f336a0dd355a625e90fbae994fbac966ed
Sahil Bansal <sahil.bansal@xoriant.com>
2020-07-18 14:01:05 +0530

    codedeploy

--- applicationstop.sh
+++ applicationstop.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 sudo rm -rf /var/www/html
+sudo rm -rf /var/www/html
+sudo rm -rf /var/www/html
+sudo rm -rf /var/www/html
 sudo mkdir -p /var/www/html
 sudo chmod -R 777 /var/www/html
