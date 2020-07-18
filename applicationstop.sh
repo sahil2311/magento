54ec01031a51974b938e1bfb500b8f2e6a2376ef
Sahil Bansal <sahil.bansal@xoriant.com>
2020-07-18 14:07:04 +0530

    codedeploy

--- applicationstop.sh
+++ applicationstop.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-sudo rm -rf /var/www/html
-sudo rm -rf /var/www/html
-sudo rm -rf /var/www/html
-sudo rm -rf /var/www/html
+DIR="/var/www/html"
+if [ -d "$DIR" ]; then
+    sudo rm -rf /var/www/html && sudo rm -rf /var/www/html
+    sudo rm -rf /var/www/html && sudo rm -rf /var/www/html
+else
+  direcoty not found.
+  exit 1
+fi
+
+if [ -d "$DIR" ]; then
+    sudo rm -rf /var/www/html && sudo rm -rf /var/www/html
+    sudo rm -rf /var/www/html && sudo rm -rf /var/www/html
+else
+  direcoty not found.
+  exit 1
+fi
+
 sudo mkdir -p /var/www/html
 sudo chmod -R 777 /var/www/html
