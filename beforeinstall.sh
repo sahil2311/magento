7ba439630143466fc98dee22627ebeded211e521
Sahil Bansal <sahil.bansal@xoriant.com>
2020-07-18 14:42:52 +0530

    applicationstop.sh

--- beforeinstall.sh
+++ beforeinstall.sh
@@ -6,15 +6,6 @@ if [ -d "$DIR" ]; then
     sudo rm -rf /var/www/html && sudo rm -rf /var/www/html
 else
   directory not found.
-  exit 1
-fi
-
-if [ -d "$DIR" ]; then
-    sudo rm -rf /var/www/html && sudo rm -rf /var/www/html
-    sudo rm -rf /var/www/html && sudo rm -rf /var/www/html
-else
-  directory not found.
-  exit 1
 fi
 
 sudo mkdir -p /var/www/html
